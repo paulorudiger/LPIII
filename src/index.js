3287726052376230e6af21b95fe56713480df7a8
paulorudiger <paulo.e.rudiger@gmail.com>
2024-03-14 21:16:09 -0300

    Adicionadas swagger tags

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 3000;
 require('./services/swagger.js')
 
+require('./routes')(app)
+app.get('/', (req, res) => { res.status(200).send('Hello World!'); });
 
-app.get('/', (req, res) => { res.send('Hello World!'); });
+app.use('/v1/docs', express.static('src/views'))
+app.use('/docs/swagger.yaml', express.static('src/docs/swagger.yaml'))
 
 app.listen(port, () => {
     console.log(`aplicação rodando na porta ${port}`);
