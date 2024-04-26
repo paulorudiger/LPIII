959b2028c18a9530c589f06a8b771e51011adaab
paulorudiger <paulo.e.rudiger@gmail.com>
2024-04-25 21:35:47 -0300

    API e BD rodando no docker

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,10 @@ RUN mkdir -p /home/node/horus
 WORKDIR /home/node/horus
 
 COPY ./package.json ./
-COPY .src .src
+COPY ./src ./src
 
 RUN npm install
 
 EXPOSE 3000
 
-CMD ["node", "./src/index.js"]
+CMD [ "node", "./src/index.js"]
--- src/configs/pg.js
+++ src/configs/pg.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'admin',
-    host: 'localhost',
+    host: 'db-horus',
     database: 'horus',
     password: '123456',
-    port: '5433'
+    port: '5432'
 });
 
 module.exports = { query: (text, params) => pool.query(text, params) }
